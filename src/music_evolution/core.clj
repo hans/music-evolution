102e5e49061ef33d2db1b7116a3a3f6272272f5d
Hans Engel <engel@engel.uk.to>
2012-06-24 13:37:48 -0700

    Mutations!

--- src/music_evolution/core.clj
+++ src/music_evolution/core.clj
@@ -13,3 +13,22 @@
   ([] (play-chord (make-chord)))
   ([chord]
      (map sin-key chord)))
+
+(defn mutate-cps
+  "Mutate a cps value."
+
+  ([cps] (mutate-cps cps 50))
+  ([cps max-delta]
+     (+ cps (- max-delta (rand-int (* 2 max-delta))))))
+
+(defn mutate-chord
+  [chord]
+  (map mutate-cps chord))
+
+(defn mutate-and-replay-chord
+  [chord]
+
+  (let [new-chord (mutate-chord chord)]
+    (stop)
+    (play-chord new-chord)
+    new-chord))
